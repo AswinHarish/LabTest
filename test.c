65077a19f236d4e8960b0dd50c6f15c0908302c8
Aswin Harish <aswinhareesh03@gmail.com>
2023-06-01 13:51:45 +0530

    Update test.c

--- test.c
+++ test.c
@@ -2,9 +2,9 @@
 
 void main()
 {
-	int a,b,c,d,e,f,g,x;
-	printf("Enter 7 numbers: ");
-	scanf("%d%d%d%d%d%d%d",&a, &b, &c, &d, &e, &f, &g);
+    int a,b,c,d,e,f,g,x;
+    printf("Enter 7 numbers: ");
+    scanf("%d%d%d%d%d%d%d",&a, &b, &c, &d, &e, &f, &g);
     x = ((a - b / c * d + e) * (f + g))
     printf("Solution is %d\n", x);
 }
