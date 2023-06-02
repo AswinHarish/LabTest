36003d15b3299ce7b32d0af8d49578221656a51a
Aswin Harish <aswinhareesh03@gmail.com>
2023-06-02 22:31:29 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,27 @@
+
 #include <stdio.h>
 
-void main()
+int main() 
 {
-    int a,b,c,d,e,f,g,x;
-    printf("Enter 7 numbers: ");
-    scanf("%d%d%d%d%d%d%d",&a, &b, &c, &d, &e, &f, &g);
-    x = ((a - b / c * d + e) * (f + g))
-    printf("Solution is %d\n", x);
+
+  int n, i;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1)
+    printf(“not a prime num\n"); 
+    return 0;
+
+  for (i = 2; i <= n / 2; i++) 
+  {
+    // if n is divisible by i, then n is not prime
+    if (n % i == 0) 
+    {
+      printf("%d is not a prime number\n", n);
+      return 0;
+    }
+  }
+  printf("%d is a prime number\n", n);
+
 }
